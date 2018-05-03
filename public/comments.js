da19350fbd33c7389079f14e0b62919cde3fb06f
lagron1000 <lagron1000@gmail.com>
2018-05-03 16:09:55 +0300

    green buttons

--- public/comments.js
+++ public/comments.js
@@ -1,19 +1,14 @@
 import { postsArray } from "./main.js";
+import { commentsClass } from "./commentsClass.js";
 
 var addComment = function(myPost, comm, name){
-    // $('.comments').empty();
-    var comment = {
-        text: comm
-    }
+    var comment = new commentsClass(comm)
+
     for (let i=0; i<postsArray.length; i++){
         if (postsArray[i].city === name){
             postsArray[i].comments.push(comment)
         }
     }
-    // comments.push(comment)
-    // for (let i=0; i<comments.length; i++){
-    //     myPost.append(comments[i].text+'<br>')
-    // }
 }
 
 export {addComment}
---
+++ public/commentsClass.js
@@ -0,0 +1,7 @@
+class commentsClass{
+    constructor(text){
+        this.text = text
+    }
+}
+
+export {commentsClass}
